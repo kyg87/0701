cda6cb41b5871b6f54783deaa169eb55f800c959
LEESEEUN <thjds8@gmail.com>
2017-04-24 16:32:47 +0900

    게시판 컨트롤러

---
+++ src/main/java/com/wiyn/web/controller/NoticeBoardController.java
@@ -0,0 +1,14 @@
+package com.wiyn.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+
+
+@Controller
+@RequestMapping("/noticeboard/*")
+public class NoticeBoardController {
+
+}
