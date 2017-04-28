2eb706ae295f5171a014cf0d4c7031e08515611f
김영균 <dridy@naver.com>
2017-04-28 16:26:28 +0900

    noticeFile & noticeBoard entity.

---
+++ src/main/java/com/wiyn/web/entity/NoticeBoard.java
@@ -0,0 +1,58 @@
+package com.wiyn.web.entity;
+
+import java.sql.Date;
+
+public class NoticeBoard {
+	
+	private String id;
+	private String title;
+	private String content;
+	private String contentSrc;
+	private int hit;
+	private Date regDate;
+	private String memberId;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getContentSrc() {
+		return contentSrc;
+	}
+	public void setContentSrc(String contentSrc) {
+		this.contentSrc = contentSrc;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	
+	
+}
---
+++ src/main/java/com/wiyn/web/entity/NoticeFile.java
@@ -0,0 +1,32 @@
+package com.wiyn.web.entity;
+
+import java.sql.Date;
+
+public class NoticeFile {
+	
+	private String id;
+	private String src;
+	private String noticeBoardId;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSrc() {
+		return src;
+	}
+	public void setSrc(String src) {
+		this.src = src;
+	}
+	public String getNoticeBoardId() {
+		return noticeBoardId;
+	}
+	public void setNoticeBoardId(String noticeBoardId) {
+		this.noticeBoardId = noticeBoardId;
+	}
+	
+	
+
+	
+}
