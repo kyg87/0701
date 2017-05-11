94a6d215432a7af2adc7745abb578c0a45bd0de3
김영균 <dridy@naver.com>
2017-05-11 17:48:38 +0900

    추가

--- src/main/java/com/wiyn/web/dao/RequestBoardDao.java
+++ src/main/java/com/wiyn/web/dao/RequestBoardDao.java
@@ -2,12 +2,20 @@ package com.wiyn.web.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Select;
+
 import com.wiyn.web.entity.RequestBoard;
 
 public interface RequestBoardDao {
 	
 	List<RequestBoard> getList();
+	
 	int add(RequestBoard requestBoard);
+	int update(RequestBoard requestBoard);
+	int delete(String memberId);
+
+	@Select("SELECT * FROM RequestBoard WHERE id=#{id}")	
+	RequestBoard get(String id);
 	
 
 	
